ef43b5898fd32f6716109274e766f5a3e16cecb4
sergejnikoleisen-lab <sergej.nikoleisen@googlemail.com>
2026-03-21 23:10:53 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import Impressum from './Impressum';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><App /></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/impressum" element={<Impressum />} />
+        <Route path="/datenschutz" element={<Impressum />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
