1abd33ea486c37177110f7b47a362f26bdd268a3
sergejnikoleisen-lab <sergej.nikoleisen@googlemail.com>
2026-03-26 03:04:58 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App';
+import App from './App-v2.jsx';
 import Impressum from './Impressum';
 import AGB from './AGB';
 
